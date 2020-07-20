dcb331b657fd21c41f66f31f34c83b5a69dcddef
Jinyeop <33533992+JinyeopSong@users.noreply.github.com>
2020-07-20 16:12:07 +0900

    Update Init_RandomSurface_AT_system.m

--- AT_system/Init_RandomSurface_AT_system.m
+++ AT_system/Init_RandomSurface_AT_system.m
@@ -77,6 +77,83 @@ if(type=="randomUniformSphere2D")
 end
 %% etc
 
+%% randomQuasiFlat2D
+if(type=="randomQuasiFlat2D")
+    p=0.5;
+    sys.type="randomUniformFlat2D";
+    sys.Tnum=floor((L)*density);
+    sys.T=(rand(1,sys.Tnum)>p);
+    sys.T_position=struct;
+    
+    p = haltonset(2,'Skip',1e3,'Leap',1e2)
+    p = scramble(p,'RR2')
+    X0 = net(p,sys.Tnum);
+
+    sys.T_position.x=(L^0.5)*X0(:,1);
+    sys.T_position.y=(L^0.5)*X0(:,2);
+    sys.T_position.z=0;
+    
+    sys.W_relation=[];
+    sys.T2W=cell(1,sys.Tnum);
+    wi=0;
+    
+    for 
+    
+    for i=1:sys.Tnum
+        for j=(i+1):sys.Tnum
+            if(DistanceBtwTwoTargets(sys,i,j)<1.01)
+                wi=wi+1;
+                sys.W_relation = [sys.W_relation ; [i,j]];
+                sys.T2W{i}=[sys.T2W{i}, wi];
+                sys.T2W{j}=[sys.T2W{j}, wi];
+            end
+        end
+    end
+    sys.W=zeros(1,size(sys.W_relation,1));
+end
+
+%% randomUniformSphere2D
+if(type=="randomUniformSphere2D")
+    p=0.5;
+    sys.type="randomUniformSphere2D";
+    sys.Tnum=floor((L)*density);
+    sys.T=(rand(1,sys.Tnum)>p);
+    sys.T_position=struct;
+    
+    
+    p = haltonset(2,'Skip',1e3,'Leap',1e2)
+    p = scramble(p,'RR2')
+    X0 = net(p,sys.Tnum);
+
+    sys.T_position.x=(L^0.5)*X0(:,1);
+    
+    theta=arccos(1-2*X0(:,1))
+    phi=2*3.141592*X0(:,1)
+    radius=(L/4/3.141592)^0.5
+    sys.T_position.x=radius*sin(theta)*cos(phi);
+    sys.T_position.y=radius*sin(theta)*sin(phi);
+    sys.T_position.z=cos(theta);
+    
+    sys.W_relation=[];
+    sys.T2W=cell(1,sys.Tnum);
+    wi=0;
+    
+    for 
+    
+    for i=1:sys.Tnum
+        for j=(i+1):sys.Tnum
+            if(DistanceBtwTwoTargets(sys,i,j)<1.01)
+                wi=wi+1;
+                sys.W_relation = [sys.W_relation ; [i,j]];
+                sys.T2W{i}=[sys.T2W{i}, wi];
+                sys.T2W{j}=[sys.T2W{j}, wi];
+            end
+        end
+    end
+    sys.W=zeros(1,size(sys.W_relation,1));
+end
+%% etc
+
 if(type=='etc')
 
 end
